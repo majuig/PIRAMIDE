5eb91651603a252ac2d5f16770ef0ad5c23ce6d3
majuig <macieljulianignacio@gmail.com>
2015-06-28 02:02:30 -0300

    piramide.py

--- piramide.py
+++ piramide.py
@@ -1,8 +1,28 @@
-#Comentario
+#Piramide
 
+class Piramide:
+    def __init__ (self, a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u):
+        self._a1 = a
+        self._a4 = b
+        self._a3 = c
+        self._a4 = d
+        self._a5 = e
+        self._a6 = f
+        self._b1 = g
+        self._b2 = h
+        self._b3 = i
+        self._b4 = j
+        self._b5 = k
+        self._c1 = l
+        self._c2 = m
+        self._c3 = n
+        self._c4 = o
+        self._d1 = p
+        self._d2 = q
+        self._d3 = r
+        self._e1 = s
+        self._e2 = t
+        self._f1 = u
 
-
-print("Hola que tal")
-
-print("Este es un cambio al archivo original")
-print("Y este otro")
+    def mostrara4():
+        print (self._a4)

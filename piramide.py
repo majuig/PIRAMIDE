89cc1480ee43197a7ab65ed7bda0cbc9e46cfe6f
majuig <macieljulianignacio@gmail.com>
2015-07-05 17:39:26 -0300

    ingreso dato con array de int

--- piramide.py
+++ piramide.py
@@ -1,5 +1,41 @@
 #Piramide
+from array import array
 
+class Piramide:
+    def __init__ (self, z):
+        self._a1 = z[0]
+        self._a4 = z[1]
+        self._a3 = z[2]
+        self._a4 = z[3]
+        self._a5 = z[4]
+        self._a6 = z[5]
+        self._b1 = z[6]
+        self._b2 = z[7]
+        self._b3 = z[8]
+        self._b4 = z[9]
+        self._b5 = z[10]
+        self._c1 = z[11]
+        self._c2 = z[12]
+        self._c3 = z[13]
+        self._c4 = z[14]
+        self._d1 = z[15]
+        self._d2 = z[16]
+        self._d3 = z[17]
+        self._e1 = z[18]
+        self._e2 = z[19]
+        self._f1 = z[20]
+
+    def mostrara4(self):
+        return self._a4
+
+
+numeros = array("i", [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21])
+test = Piramide(numeros)
+print(test.mostrara4())
+
+
+
+"""
 class Piramide:
     def __init__ (self, a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u):
         self._a1 = a
@@ -24,20 +60,8 @@ class Piramide:
         self._e2 = t
         self._f1 = u
 
-    def mostrara4(self):
-        return self._a4
 
-test = Piramide(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21)
-print(test.mostrara4())
-
-
-
-class animal():
-    def __init__(self,tipo):
-        self._tipo = tipo
-
-    def mostrar(self):
-        return self._tipo
-
-Rocky = animal("perro")
-print(Rocky.mostrar())
+    git add piramide.py
+    git commit -m "comentario"
+    git push origin master
+"""
